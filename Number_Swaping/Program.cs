cfce295f4c440bbf301773ced63efc6c8d95cd1e
ErrorX <31070311+Nalox1@users.noreply.github.com>
2020-04-26 14:19:13 +0200

    Add files via upload

---
+++ Number_Swaping/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Number_Swaping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Swaping to numbers
+            int num1, num2, extra;
+            Console.WriteLine("Enter first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter secound number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("You enterd: " + num1 + " " + num2);
+
+            extra = num1;
+            num1 = num2;
+            num2 = extra;
+            Console.WriteLine("Your numbers are swaped: " + num1 + " " + num2);
+        }
+    }
+}
